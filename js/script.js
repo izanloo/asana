b9ec4d7795597c625079880bbe4a21a4b583c925
araks1397 <razieh.izanloo755@gmail.com>
2019-05-31 17:22:46 +0430

    responsive 768px

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.getElementById("nav");
+    var element1=document.getElementById("serchh");
+    var element2=document.getElementById("chearr");
+    element.classList.toggle("toggle-menu");
+    element1.classList.toggle("serch2");
+    element2.classList.toggle("chear1");
+}
